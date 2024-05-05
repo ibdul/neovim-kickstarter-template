87f73388d22d7668f5a645bb0f1f384786784200
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-05-05 21:12:33 +0100

    disable twilight

--- lua/custom/plugins/twilight.lua
+++ lua/custom/plugins/twilight.lua
@@ -1,8 +1,8 @@
-return {
+local m = {
 	"folke/twilight.nvim",
 	opts = {
 		dimming = {
-			alpha = 0.25, -- amount of dimming
+			alpha = 0.1, -- amount of dimming
 			-- we try to get the foreground from the highlight groups or fallback color
 			color = { "Normal", "#ffffff" },
 			term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
@@ -25,3 +25,5 @@ return {
 		vim.cmd("TwilightEnable")
 	end
 }
+
+return {}
